8b1a2173cb116ccaa13614b74645131d4da2f511
Hemapriya <hemapriyalakshmi05@gmail .com>
2026-05-27 12:24:54 +0530

    add hamburger

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import "bootstrap-icons/font/bootstrap-icons.css";
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
   
   return (
     <>
-    <Navbar/>  
+    <Navbar/>
     </>
   )
 }
