616ccfc6e6e10377b07f2cac4a928edc66d6da68
yuvapriyab-4 <yuvapriyab4@gmail.com>
2026-05-29 11:47:07 +0530

    change the code

--- src/sections/packages/packages.jsx
+++ src/sections/packages/packages.jsx
@@ -1,1 +1,21 @@
 import React from "react";
+import "./packages.css";
+
+function Packages() {
+    return(
+        <section className="packages-section">
+            <h4> Our Tour Packages </h4>
+            <h1> Popular Tour Packages </h1>
+
+            <div className="packages-container">
+                <div className="package-card">
+                    <img src="/assets/images/package1.jpg" alt="Package 1" className="package-image" />
+                    <h3>VAGAMON</h3>
+                    <p>Explore Adventures Places</p>
+
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
